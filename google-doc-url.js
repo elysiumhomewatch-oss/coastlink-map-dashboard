8051d7a0e7767be641075f25c76eac7ee02bb9fd
elysiumhomewatch-oss <elysiumhomewatch@gmail.com>
2026-02-16 22:33:07 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Bi9g1mnhkHFqqvNhIwQkikJ2gDkJx5xk5jheyu0kAE4/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Bi9g1mnhkHFqqvNhIwQkikJ2gDkJx5xk5jheyu0kAE4/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyDx47uiLymMFYOi8Cvi-285iR1LTjaY02A';
